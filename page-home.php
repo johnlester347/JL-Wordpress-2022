0412cf2c764e983a49adf24ddf96486b5dae6561
John Lester <johnlesterbalucas.pamplona@gmail.com>
2022-02-11 12:58:16 +0700

    feat: added feature section

--- page-home.php
+++ page-home.php
@@ -18,24 +18,35 @@
  $optin_btn_txt     = get_post_meta(11, 'optin_btn_txt', true);
 
  // Advanced Custom Field
- $income_feature_image      = get_field('income_feature_image');
- $income_section_title      = get_field('income_section_title');
- $income_section_desc       = get_field('income_section_description');
- $reason_1_title            = get_field('reason_1_title');
- $reason_1_desc             = get_field('reason_1_description');
- $reason_2_title            = get_field('reason_2_title');
- $reason_2_desc             = get_field('reason_2_description');
+ $income_feature_image          = get_field('income_feature_image');
+ $income_section_title          = get_field('income_section_title');
+ $income_section_desc           = get_field('income_section_description');
+ $reason_1_title                = get_field('reason_1_title');
+ $reason_1_desc                 = get_field('reason_1_description');
+ $reason_2_title                = get_field('reason_2_title');
+ $reason_2_desc                 = get_field('reason_2_description');
 
- $who_featured_image        = get_field('who_feature_image');
- $who_section_title        = get_field('who_section_title');
- $who_section_body        = get_field('who_section_body');
+ $who_featured_image            = get_field('who_feature_image');
+ $who_section_title             = get_field('who_section_title');
+ $who_section_body              = get_field('who_section_body');
 
- $features_section_image   = get_field('features_section_image');
- $features_section_title   = get_field('feature_section_title');
- $features_section_body   = get_field('feature_section_body');
+ $features_section_image        = get_field('features_section_image');
+ $features_section_title        = get_field('feature_section_title');
+ $features_section_body         = get_field('feature_section_body');
 
- $project_feature_title     = get_field('project_feature_title');
- $project_feature_body     = get_field('project_feature_body');
+ $project_feature_title         = get_field('project_feature_title');
+ $project_feature_body          = get_field('project_feature_body');
+
+ $instructor_section_title      = get_field('instructor_section_title');
+ $instructor_name               = get_field('instructor_name');
+ $bio_excerpt                   = get_field('bio_excerpt');
+ $full_bio                      = get_field('full_bio');
+ $twitter_username              = get_field('twitter_username');
+ $facebook_username             = get_field('facebook_username');
+ $google_plus_username          = get_field('google_plus_username');
+ $num_students                  = get_field('num_students');
+ $num_reviews                   = get_field('num_reviews');
+ $num_courses                   = get_field('num_courses');
 
 get_header();
 ?>
@@ -247,36 +258,33 @@ get_header();
             <div class="col-sm-8 col-md-6">
                 <div class="row">
                     <div class="col-lg-8">
-                        <h2>Your Instructor <small>Brad Hussey</small></h2>
+                        <h2><?php echo $instructor_section_title ?><br><small><?php echo $instructor_name; ?></small>
+                        </h2>
                     </div><!-- end col -->
                     <div class="col-lg-4">
-                        <a href="https://twitter.com/bradhussey" class="badge social twitter" target="_blank"><i
-                                class="fa fa-twitter"></i></a>
-                        <a href="https://facebook.com/bradhussey" class="badge social facebook" target="_blank"><i
-                                class="fa fa-facebook"></i></a>
-                        <a href="https://plus.google.com/+BradHussey" class="badge social gplus" target="_blank"><i
-                                class="fa fa-google-plus"></i></a>
+                        <?php if(!empty($twitter_username)) : ?>
+                        <a href="https://twitter.com/<?php echo $twitter_username; ?>" class="badge social twitter"
+                            target="_blank"><i class="fa fa-twitter"></i></a>
+                        <?php endif; ?>
+
+                        <?php if(!empty($facebook_username)) : ?>
+                        <a href="https://facebook.com/<?php echo $facebook_username; ?>" class="badge social facebook"
+                            target="_blank"><i class="fa fa-facebook"></i></a>
+                        <?php endif; ?>
+
+                        <?php if(!empty($google_plus_username)) : ?>
+                        <a href="https://plus.google.com/<?php echo $google_plus_username; ?>"
+                            class="badge social gplus" target="_blank"><i class="fa fa-google-plus"></i></a>
+                        <?php endif; ?>
+
                     </div><!-- end col -->
 
                 </div><!-- row -->
 
-                <p class="lead">A highly skilled professional, Brad Hussey is a passionate and experienced web
-                    designer, developer, blogger and digital entrepreneur.
+                <p class="lead"><?php echo $bio_excerpt; ?>
                 <p>
 
-                <p>Hailing from North Of The Wall (Yellowknife, Canada), Brad made the trek to the Wet Coast
-                    (Vancouver, Canada) to educate and equip himself with the necessary skills to become a spearhead
-                    in his trade of solving problems on the web, crafting design solutions, and speaking in code.
-                </p>
-
-                <p>Brad's determination and love for what he does has landed him in some pretty interesting places
-                    with some neat people. He's had the privilege of working with, and providing solutions for,
-                    numerous businesses, big &amp; small, across the Americas.</p>
-
-                <p>Brad builds custom websites, and provides design solutions for a wide-array of clientele at his
-                    company, Brightside Studios. He regularly blogs about passive income, living your life to the
-                    fullest, and provides premium quality web design tutorials and courses for tens of thousands of
-                    amazing people desiring to master their craft.</p>
+                <p><?php echo $full_bio; ?></p>
 
                 <hr>
 
@@ -285,7 +293,7 @@ get_header();
                     <div class="col-xs-4">
                         <div class="num">
                             <div class="num-content">
-                                41,000+ <span>students</span>
+                                <?php echo $num_students; ?><span>students</span>
                             </div><!-- num-content -->
                         </div><!-- num -->
                     </div><!-- end col -->
@@ -293,7 +301,7 @@ get_header();
                     <div class="col-xs-4">
                         <div class="num">
                             <div class="num-content">
-                                568 <span>reviews</span>
+                                <?php echo $num_reviews; ?> <span>reviews</span>
                             </div><!-- num-content -->
                         </div><!-- num -->
                     </div><!-- end col -->
@@ -301,7 +309,7 @@ get_header();
                     <div class="col-xs-4">
                         <div class="num">
                             <div class="num-content">
-                                8 <span>courses</span>
+                                <?php echo $num_courses; ?> <span>courses</span>
                             </div><!-- num-content -->
                         </div><!-- num -->
                     </div><!-- end col -->

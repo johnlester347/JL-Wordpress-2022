b6e905a6046d6561c4808034b3fd783fe9cfe15c
John Lester <johnlesterbalucas.pamplona@gmail.com>
2022-02-13 02:14:51 +0700

    feat: added blog content

--- functions.php
+++ functions.php
@@ -193,3 +193,23 @@ if ( defined( 'JETPACK__VERSION' ) ) {
 // }
 
 add_image_size('blog-small', 200, 200, false);
+
+/**
+ * Replaces the excerpt "more" text by a link.
+ */
+function new_excerpt_more($more) {
+    global $post;
+	return '... <a class="moretag" href="'. get_permalink($post->ID) . '"> continue reading &raquo;</a>';
+}
+add_filter('excerpt_more', 'new_excerpt_more');
+
+/**
+ * Filter the except length to 20 words.
+ *
+ * @param int $length Excerpt length.
+ * @return int (Maybe) modified excerpt length.
+ */
+function wpdocs_custom_excerpt_length( $length ) {
+    return 30;
+}
+add_filter( 'excerpt_length', 'wpdocs_custom_excerpt_length', 999 );
--- template-parts/content.php
+++ template-parts/content.php
@@ -10,54 +10,72 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php
+    <header class="entry-header">
+        <?php
 		if ( is_singular() ) :
 			the_title( '<h1 class="entry-title">', '</h1>' );
 		else :
-			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+			the_title( '<h3 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h3>' );
 		endif;
 
 		if ( 'post' === get_post_type() ) :
 			?>
-			<div class="entry-meta">
-				<?php
-				bootstrap2wordpress_posted_on();
-				bootstrap2wordpress_posted_by();
-				?>
-			</div><!-- .entry-meta -->
-		<?php endif; ?>
-	</header><!-- .entry-header -->
+        <div class="post-details">
+            <i class="fa fa-user"></i> <?php the_author(); ?>
+            <i class="fa fa-clock-o"></i> <time><?php the_date(); ?></time>
+            <i class="fa fa-folder"></i><?php the_category(', '); ?>
+            <i class="fa fa-tags"></i> <?php the_tags('', ', ', ''); ?>
 
-	<?php bootstrap2wordpress_post_thumbnail(); ?>
+            <div class="post-comments-badge">
+                <a href="<?php comment_link(); ?>"><i class="fa fa-comments"></i><?php comments_number(0,1,'%'); ?></a>
+            </div><!-- post-comments-badge -->
+            <?php edit_post_link('Edit', '<div><i class="fa fa-comments"></i>', '</div>'); ?>
+        </div><!-- post-details -->
+        <?php endif; ?>
+    </header><!-- .entry-header -->
 
-	<div class="entry-content">
-		<?php
-		the_content(
-			sprintf(
-				wp_kses(
-					/* translators: %s: Name of current post. Only visible to screen readers */
-					__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'bootstrap2wordpress' ),
-					array(
-						'span' => array(
-							'class' => array(),
-						),
-					)
-				),
-				wp_kses_post( get_the_title() )
-			)
-		);
+    <?php if (has_post_thumbnail()) : ?>
+    <div class="post-image">
+        <?php the_post_thumbnail(); ?>
+    </div><!-- post-image -->
+    <?php endif; ?>
+    <div class="post-excerpt">
+        <p><?php the_excerpt(  ); ?></p>
+    </div><!-- post-excerpt -->
 
-		wp_link_pages(
-			array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'bootstrap2wordpress' ),
-				'after'  => '</div>',
-			)
-		);
+
+    <!----------- BACKUP COPY OF INDEX CONTNENT ----------------->
+    <?php //bootstrap2wordpress_post_thumbnail(); ?>
+
+    <!-- <div class="entry-content"> -->
+    <?php
+		// the_content(
+		// 	sprintf(
+		// 		wp_kses(
+		// 			/* translators: %s: Name of current post. Only visible to screen readers */
+		// 			__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'bootstrap2wordpress' ),
+		// 			array(
+		// 				'span' => array(
+		// 					'class' => array(),
+		// 				),
+		// 			)
+		// 		),
+		// 		wp_kses_post( get_the_title() )
+		// 	)
+		// );
+
+		// wp_link_pages(
+		// 	array(
+		// 		'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'bootstrap2wordpress' ),
+		// 		'after'  => '</div>',
+		// 	)
+		// );
 		?>
-	</div><!-- .entry-content -->
+    <!-- </div> -->
 
-	<footer class="entry-footer">
-		<?php bootstrap2wordpress_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
+    <!-- <footer class="entry-footer"> -->
+    <?php //bootstrap2wordpress_entry_footer(); ?>
+    <!-- </footer> -->
+    <!----------- BACKUP COPY OF INDEX CONTNENT ----------------->
+
 </article><!-- #post-<?php the_ID(); ?> -->

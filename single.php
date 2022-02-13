52d7c290cf0a534143e128a9816ad4a907aa9fc0
John Lester <johnlesterbalucas.pamplona@gmail.com>
2022-02-13 13:37:12 +0700

    feat: add comment stylings

--- single.php
+++ single.php
@@ -10,23 +10,24 @@
 get_header();
 ?>
 
+<!-- BLOG CONTENT
+================================================== -->
 <div class="container">
     <div class="row" id="primary">
-
         <main id="content" class="col-sm-8">
 
             <?php
-		while ( have_posts() ) :
-			the_post();
+		    while ( have_posts() ) :
+                the_post();
 
-			get_template_part( 'template-parts/content', get_post_type() );
+                get_template_part( 'template-parts/content', get_post_type() );
 
-			the_post_navigation(
-				array(
-					'prev_text' => '<span class="nav-subtitle">' . esc_html__( 'Previous:', 'bootstrap2wordpress' ) . '</span> <span class="nav-title">%title</span>',
-					'next_text' => '<span class="nav-subtitle">' . esc_html__( 'Next:', 'bootstrap2wordpress' ) . '</span> <span class="nav-title">%title</span>',
-				)
-			);
+                the_post_navigation(
+                    array(
+                        'prev_text' => '<span class="nav-subtitle">' . esc_html__( 'Previous:', 'bootstrap2wordpress' ) . '</span> <span class="nav-title">%title</span>',
+                        'next_text' => '<span class="nav-subtitle">' . esc_html__( 'Next:', 'bootstrap2wordpress' ) . '</span> <span class="nav-title">%title</span>',
+                    )
+                );
 
 			// If comments are open or we have at least one comment, load up the comment template.
 			if ( comments_open() || get_comments_number() ) :
@@ -36,6 +37,9 @@ get_header();
 		endwhile; // End of the loop.
 		?>
         </main>
+
+        <!-- SIDEBAR
+			================================================== -->
         <aside class="col-sm-4">
             <?php get_sidebar(); ?>
         </aside>
